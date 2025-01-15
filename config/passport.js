893b986cdf93b4789053a41545673fce1c71ed74
Nero <karta0706566@gmail.com>
2025-01-15 21:40:29 +0800

    feat: add signin feature

---
+++ config/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport')
+const JWTSTrategy = require('passport-jwt').Strategy
+const ExtractJWT = require('passport-jwt').ExtractJwt
+const { User } = require('../models')
+
+const Options = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET
+}
+
+passport.use(new JWTSTrategy(Options, async(payload, cb)=>{
+  try{
+    const user = await User.findByPk(payload.id)
+    return (null,user)
+  } catch(err){
+    return cb(err)
+  }
+}))
+
+module.exports = passport
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 };
 
@@ -6,9 +6,28 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use(express.json())
-app.listen(port, ()=>console.log(`Your app is running on port: ${port}!`))
+const bcrypt = require('bcryptjs')
+const { User } = require('./models')
 
-app.post('/signin',(req,res,next)=>{
+app.use(express.json())
+app.listen(port, () => console.log(`Your app is running on port: ${port}!`))
+
+app.post('/signin', async (req, res, next) => {
+  const { account, password } = req.body
+
+  try {
+    const user = await User.findOne({ where: { account }, raw: true })
+    if (!user) throw new Error('Incorrect account or password.')
+
+    const userCheck = await bcrypt.compare(password, user.password)
+    if (!userCheck) throw new Error('Incorrect account or password.')
+
+    delete user.password
+    const token = jwt.sign(user, process.env.SECRET, { expiresIn: '1d' })
+
+    return res.json({ status: 200, message: 'Signin successfully', token })
+  } catch (err) {
+
+  }
   return res.send("hello world!")
 })
